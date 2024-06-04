e15982fe93ee1c7ccfa64e2eb6ae6131fe629a66
camiurenda <urendacamila@gmail.com>
2024-06-04 21:14:08 +0200

    Esqueleto de como deberia quedar

---
+++ Backend/middleware/auth.js
@@ -0,0 +1,8 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const auth = async (req, res, next) => {
+  //aca iria la lógica de autenticación con JWT
+};
+
+module.exports = auth;
