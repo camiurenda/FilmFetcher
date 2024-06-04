d0c627696b500c57eb2ee461cfd761b6b7502833
camiurenda <urendacamila@gmail.com>
2024-06-04 20:55:05 +0200

    Agregado de dotenv, puertos y rutas

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
