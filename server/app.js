10c0dcdd5a8a4b22cdf77200609080388cda975f
camiurenda <urendacamila@gmail.com>
2024-10-14 20:20:31 -0300

    Quitar param env redundante

--- server/app.js
+++ server/app.js
@@ -37,7 +37,6 @@ app.use(auth(config));
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5000',
-  process.env.FRONTEND_URL,
   'https://film-fetcher-eta.vercel.app',
   'https://film-fetcher-exc9.vercel.app'
 ];
