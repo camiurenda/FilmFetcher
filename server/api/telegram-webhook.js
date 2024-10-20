d777292ef754d3ffe488e8f25b58a429d280ac58
camiurenda <urendacamila@gmail.com>
2024-10-19 23:13:16 -0300

    Comentar el hook para ver si ahi esta el problema

--- server/api/telegram-webhook.js
+++ server/api/telegram-webhook.js
@@ -1,15 +1,15 @@
-const TelegramService = require('../services/telegram.service');
+// const TelegramService = require('../services/telegram.service');
 
-module.exports = async (req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Método no permitido' });
-  }
+// module.exports = async (req, res) => {
+//   if (req.method !== 'POST') {
+//     return res.status(405).json({ error: 'Método no permitido' });
+//   }
 
-  try {
-    await TelegramService.handleUpdate(req.body);
-    res.status(200).json({ message: 'OK' });
-  } catch (error) {
-    console.error('Error al procesar la actualización de Telegram:', error);
-    res.status(500).json({ error: 'Error interno del servidor' });
-  }
-};
+//   try {
+//     await TelegramService.handleUpdate(req.body);
+//     res.status(200).json({ message: 'OK' });
+//   } catch (error) {
+//     console.error('Error al procesar la actualización de Telegram:', error);
+//     res.status(500).json({ error: 'Error interno del servidor' });
+//   }
+// };
