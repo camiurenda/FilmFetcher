fe260d613743aaf109a7c10feebfae2fa8482d90
camiurenda <urendacamila@gmail.com>
2024-12-12 20:45:15 -0300

    va cambio que anda

--- server/services/imagescraping.service.js
+++ server/services/imagescraping.service.js
@@ -155,7 +155,7 @@ class ImageScrapingService {
       const response = await axios.post(
         'https://api.openai.com/v1/chat/completions',
         {
-          model: "gpt-4o",
+          model: "gpt-4o-mini",
           messages: [
             {
               role: "system",
