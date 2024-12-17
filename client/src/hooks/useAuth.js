35245267f94bf0a908d221ae2fcdba4caf2010a6
camiurenda <urendacamila@gmail.com>
2024-12-17 20:01:38 -0300

    OTRA OPCION

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -7,6 +7,9 @@ const whitelistedEmails = [
   'sigiliosello@gmail.com'
 ];
 
+const PRODUCTION_URL = 'https://film-fetcher-eta.vercel.app';
+const DEVELOPMENT_URL = 'http://localhost:3000';
+
 export const useAuth = () => {
   const { loginWithRedirect, logout, user, isAuthenticated, isLoading } = useAuth0();
   const [isAuthorized, setIsAuthorized] = useState(false);
@@ -20,7 +23,9 @@ export const useAuth = () => {
   const handleLogin = () => loginWithRedirect();
   
   const handleLogout = () => {
-    const redirectUri = `${window.location.origin}`;
+    // Determinar si estamos en producción o desarrollo
+    const isProd = window.location.origin.includes('vercel.app');
+    const redirectUri = isProd ? PRODUCTION_URL : DEVELOPMENT_URL;
     console.log('Redirigiendo a:', redirectUri);
     logout({ returnTo: redirectUri });
   };
